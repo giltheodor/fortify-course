decbba647b0cafb3b6f7c32664ce0ace8cf12a51
giltheodor <giltheod@gmail.com>
2022-08-13 14:40:12 +0300

    Last move

--- src/js/controller.js
+++ src/js/controller.js
@@ -128,5 +128,6 @@ const init = function () {
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
   console.log('Welcome');
+  console.log('Last command');
 };
 init();
